5e254601679ea7d7a6fa36b8ca772238188cedad
User <user@user.ru>
2023-11-20 20:15:57 +0300

    lesson_23

---
+++ lesson_23 (oop pattern)/lesson_23.py
@@ -0,0 +1,7 @@
+# S - (S...) принцип единства ответственности
+# O - (OCP - The open-closed principle) принцип открытости-закрытости
+# L - (LSP - The Liskov Substitution Principle) принцип подстановки Лисков
+# I - (ISP) принцип разделения интерфейсов
+# D - (DIP - The Dependency Inversion Principle) принцип
+
+
