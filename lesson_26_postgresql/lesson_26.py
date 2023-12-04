49f57f28ad3a022f96e34ea6679c5e31a99bef4c
User <user@user.ru>
2023-12-04 22:12:24 +0300

    lesson_26

--- lesson_26_postgresql/lesson_26.py
+++ lesson_26_postgresql/lesson_26.py
@@ -1,3 +1,8 @@
+# БД - структурированный набор данных
+# Реляционные бд - бд, в которых есть связи между данными
+# Всегда должен быть Primary key (Лучше это будет ID) (не повторяются)
+# Super key - состоит из нескольких Primary key
+# Foreign key - внешние ключи
 # CREATE DATABASE sample;    # писать заглавными буквами, в конце всегда точка с запятой
 # ALTER DATABASE sample RENAME TO example;
 # CREATE TABLE "StudentGrades" (
@@ -11,3 +16,5 @@
 # INSERT INTO "StudentGrades" ("StudentName") VALUES ('Ivan Ivanov');    # Добавить строку
 # DELETE FROM "StudentGrades" WHERE "StudenName" = 'Ivan Ivanov';    # Удаление значения
 # UPDATE "StudentGrades" set "PhoneNumber" = '79999999999' "City" = 'Moscow' WHERE "StudentName" = 'Ivan Ivanov';
+# SELECT "StudentName" FROM "StudentGrades" WHERE "AverageGrade" BETWEEN 3 AND 4.5;    # Сравнение
+
